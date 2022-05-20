40ff6a4dc391add8bdced456e4c153bacdc696e1
Tadres <nicolas.tadres@edu.esiee.fr>
2022-05-20 17:46:33 +0200

    Inverser un mélange + test

--- src/Cube.ts
+++ src/Cube.ts
@@ -458,7 +458,7 @@ class Cube {
    }
 
    public randomMove(){
-      var move = "";
+      let move = "";
       const allMove = ["R", "R'", "R2", "L", "L'", "L2", "F", "F'", "F2", "B", "B'", "B2", "U", "U'", "U2", "D", "D'", "D2"];
       const nb = Math.floor(Math.random()*(Math.floor(30) - Math.ceil(20) + 1) + Math.ceil(20));
       for(let i=0; i<nb; i++){
@@ -467,6 +467,56 @@ class Cube {
       }
       return move;
    }
+
+   public inverseScramble(scramble: string){
+      const currentScramble = scramble.split(" ");
+
+      for (let i=0; i<currentScramble.length; i++){
+         switch(currentScramble[i]){
+            case "R":
+               currentScramble[i] = "R'";
+               break;
+            case "R'":
+               currentScramble[i] = "R";
+               break;
+            case "L":
+               currentScramble[i] = "L'";
+               break;
+            case "L'":
+               currentScramble[i] = "L";
+               break;
+            case "F":
+               currentScramble[i] = "F'";
+               break;
+            case "F'":
+               currentScramble[i] = "F";
+               break;
+            case "B":
+               currentScramble[i] = "B'";
+               break;
+            case "B'":
+               currentScramble[i] = "B";
+               break;
+            case "U":
+               currentScramble[i] = "U'";
+               break;
+            case "U'":
+               currentScramble[i] = "U";
+               break;
+            case "D":
+               currentScramble[i] = "D'";
+               break;
+            case "D'":
+               currentScramble[i] = "D";
+               break;
+         }
+      }
+      const reverseScramble = currentScramble.reverse();
+      scramble = reverseScramble.join(' ');
+
+      return scramble;
+   }
+
 
 }
 
--- src/main.ts
+++ src/main.ts
@@ -12,15 +12,35 @@ const cube = new Cube([
    Array(9).fill(Yellow),
 ]);
 
-console.table(cube.tableau);
+for(let i=0; i<100000; i++){
+   const a = cube.randomMove();
+   if (cube.move(a)!== cube.move(cube.inverseScramble(a))){
+      console.log(false);
+   }
+   else{console.log(true)}
+}
 
-const a = cube.randomMove();
 
-console.log(a);
+//console.table(cube.tableau);
 
-cube.move(a);
+//const a = cube.randomMove();
 
-console.table(cube.tableau);
+//console.log(a);
+
+//cube.move(a);
+
+//console.table(cube.tableau);
+
+//const b = cube.inverseScramble(a)
+
+//console.log(b)
+
+//cube.move(b);
+
+//console.table(cube.tableau);
+
+
+
 
 
 
